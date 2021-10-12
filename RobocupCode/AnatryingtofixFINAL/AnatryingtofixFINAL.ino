dff0d94f2f143d605b88be93bfdad8bdacbe8ea5
Anahita Piri <api48@uclive.ac.nz>
2021-10-12 22:26:04 +1300

    why

--- RobocupCode/AnatryingtofixFINAL/AnatryingtofixFINAL.ino
+++ RobocupCode/AnatryingtofixFINAL/AnatryingtofixFINAL.ino
@@ -37,13 +37,18 @@ SharpIR frontr( SharpIR::GP2Y0A21YK0F, A2 );
 SharpIR Mr( SharpIR::GP2Y0A21YK0F, A4 );
 SharpIR Ml( SharpIR::GP2Y0A21YK0F, A5 );
 
+int redUp;
+int greenUp;
+int blueUp;
+int redLow;
+int greenLow;
+int blueLow;
 
 MedianFilter<int> medianFilter(30);
 MedianFilter<int> median1Filter(2);
 MedianFilter<int> median2Filter(2);
 MedianFilter<int> median3Filter(2);
 MedianFilter<int> median4Filter(2);
-
 
 MedianFilter<int> mediantrFilter(10);
 MedianFilter<int> medianMrFilter(10);
@@ -53,7 +58,7 @@ MedianFilter<int> medianMlFilter(10);
 Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_614MS, TCS34725_GAIN_1X);
 uint16_t r, g, b, c, colorTemp, lux;
 int timecounter = 0;
-int watchdogtimer = 0;
+
 //**********************************************************************************
 // Local Definitions
 //**********************************************************************************
@@ -62,11 +67,6 @@ int watchdogtimer = 0;
 int mirighttSensor = A5;
 int mideftSensor = A4;
 
-long compare_right;
-long compare_left;
-
-float newYaw = 0;
-float oldYaw = 0;
 bool base = 1; //1 for red base, 0 for green
 bool onBase = false;
 //**********************************************************************************
@@ -77,59 +77,32 @@ bool onBase = false;
 #define IO_POWER  49
 #define BAUD_RATE 9600
 
-
 bool backflag = 0;
 int moveSpeed = 100;
 int turn = 0;
 
 #define FRONTLIMIT 15
-#define CORNERLIMIT 20
-#define SIDELIMIT 13
+#define CORNERLIMIT 25
+#define SIDELIMIT 18
 
 // IMU stuff
-
+float newYaw = 0;
+float oldYaw = 0;
 float yaw;
-int yawChange;
 float watchdog = 0;
-int colourdiff = 30;
+int watchdogtimer = 0;
+float roll;
+
 //**********************************************************************************
 // Function Definitions
 //**********************************************************************************
 void pin_init();
 int homeboi;
-//**********************************************************************************
-// SETUP
-//**********************************************************************************
-void setup() {
 
-  pin_init();
-  weightservo_setup();
-  initServo();
-  motor_setup();
-  randomSeed(analogRead(7));
+#define STGREEN 1
+#define STBLUE 2
 
-  Serial.begin(9600);
-  bno.reset();
-  while (bno.begin() != BNO::eStatusOK) {
-    Serial.println("bno begin faild");
-    printLastOperateStatus(bno.lastOperateStatus);
-    delay(2000);
-  }
-  Serial.println("bno begin success");
-
-
-
-
-  tcs.getRawData(&r, &g, &b, &c);
-  delay(10);
-  tcs.getRawData(&r, &g, &b, &c);
-int redUP = r + colourdiff;
-int greenUp = g + colourdiff;
-int blueUp = b + colourdiff;
-int redLow = r - colourdiff;
-int greenLow = g - colourdiff;
-int blueLow = b - colourdiff;
-}
+int colstart = 0;
 
 //**********************************************************************************
 // Initialise the pins as inputs and outputs (otherwise, they won't work)
@@ -155,20 +128,50 @@ void pin_init() {
 //    Serial.println("Robot is ready \n");
 //}
 
+int watchdogboi;
+//**********************************************************************************
+// SETUP
+//**********************************************************************************
+void setup() {
+  timecounter += 1;
+  pin_init();
+  weightservo_setup();
+  initServo();
+  motor_setup();
+  randomSeed(analogRead(7));
 
+  Serial.begin(9600);
+  bno.reset();
+  while (bno.begin() != BNO::eStatusOK) {
+    Serial.println("bno begin faild");
+    printLastOperateStatus(bno.lastOperateStatus);
+    delay(2000);
+  }
+  Serial.println("bno begin success");
 
+  tcs.getRawData(&r, &g, &b, &c);
+  delay(10);
+  tcs.getRawData(&r, &g, &b, &c);
+  int colourdiff = 30;
+  int redUp = r + colourdiff;
+  int greenUp = g + colourdiff;
+  int blueUp = b + colourdiff;
+  int redLow = r - colourdiff;
+  int greenLow = g - colourdiff;
+  int blueLow = b - colourdiff;
+  if (g > b) {
+    colstart = STGREEN;
+  } else {
+    colstart = STBLUE;
+  }
+}
 unsigned long timeboi;
+
 void loop() {
   timeboi = millis();
   collect_weight();
   Serial.println(timeboi);
 
-    Serial.print("R: "); Serial.print(r, DEC); Serial.print(" ");
-  Serial.print("G: "); Serial.print(g, DEC); Serial.print(" ");
-  Serial.print("B: "); Serial.print(b, DEC); Serial.print(" ");
-//  Serial.print("C: "); Serial.print(c, DEC); Serial.print(" ");
-  Serial.println(" ");
-  //
   int rightDistance = right.getDistance();
   int leftDistance = left.getDistance();
   int frontDistancer = frontr.getDistance();
@@ -182,24 +185,12 @@ void loop() {
   MlDistance = medianMlFilter.AddValue(MlDistance);
   MrDistance = medianMrFilter.AddValue(MrDistance);
 
-  //  Serial.print("right: ");
-  //  Serial.print(rightDistance);
-  //  Serial.print(" left: ");
-  //  Serial.print(leftDistance);
-  //  Serial.print(" front right: ");
-  //  Serial.print(frontDistancer);
-  //  Serial.print(" front left: ");
-  //  Serial.print(frontDistancel);
-  //  Serial.print("Mid Left: ");
-  //  Serial.print(MlDistance);
-  //  Serial.print(", ");
-  //  Serial.print("Mid right: ");
-  //  Serial.println(MrDistance);
-
+  ///////////////////////////////////////IF STATEMENT 1- No sensors triggered everything clear//////////////////////////////////////////////////
   if (rightDistance > SIDELIMIT && leftDistance > SIDELIMIT && frontDistancer > FRONTLIMIT && frontDistancel > FRONTLIMIT && MlDistance > CORNERLIMIT && MrDistance > CORNERLIMIT) {
     go_forward(moveSpeed);
   }
-  else if ( frontDistancer < FRONTLIMIT && frontDistancel < FRONTLIMIT && MlDistance < CORNERLIMIT && MrDistance < CORNERLIMIT) {
+  ///////////////////////////////////////IF STATEMENT 2 - Front four detected//////////////////////////////////////////////////
+  else if (frontDistancer < FRONTLIMIT && frontDistancel < FRONTLIMIT && MlDistance < CORNERLIMIT && MrDistance < CORNERLIMIT) {
     rightDistance = right.getDistance();
     leftDistance = left.getDistance();
     if ((rightDistance < SIDELIMIT)) {
@@ -211,55 +202,80 @@ void loop() {
     else {
       go_back();
       delay(100);
-      turn_right(moveSpeed);
+
+      if (colstart == STGREEN) {
+        turn_right(moveSpeed);
+      } else {
+        turn_left(moveSpeed);
+      }
       delay(100);
-
-
     }
-
   }
+
+    ///////////////////////////////////////IF STATEMENT 3 - Two corner ones triggered, It is probbaly in a corner//////////////////////////////////////////////////
+
+  else if ((frontDistancer < FRONTLIMIT) && (frontDistancel < FRONTLIMIT)) {
+    turn_left(moveSpeed);
+    delay(400);
+                            
+  }
+  
+  ///////////////////////////////////////IF STATEMENT 3 - Two corner ones triggered, It is probbaly in a corner//////////////////////////////////////////////////
+
   else if ((MlDistance < CORNERLIMIT) && (MrDistance < CORNERLIMIT)) {
     turn_left(moveSpeed);
     delay(400);
   }
 
+  ///////////////////////////////////////IF STATEMENT 4 - If right one triggered only, probably moving straight along a wall.//////////////////////////////////////////////////
+
   else if (rightDistance < SIDELIMIT) {
     turn_left(moveSpeed);
 
   }
+  ///////////////////////////////////////IF STATEMENT 5//////////////////////////////////////////////////
 
   else if (MrDistance < CORNERLIMIT) {
     turn_left(moveSpeed);
 
   }
 
+  ///////////////////////////////////////IF STATEMENT 6//////////////////////////////////////////////////
+
+  else if (leftDistance < SIDELIMIT) {
+    turn_right(moveSpeed);
+  }
+  ///////////////////////////////////////IF STATEMENT 7//////////////////////////////////////////////////
+
+  else if (MlDistance < CORNERLIMIT) {
+    turn_right(moveSpeed);
+
+  }
+  ///////////////////////////////////////IF STATEMENT 8//////////////////////////////////////////////////
+
   else if (MrDistance < CORNERLIMIT && rightDistance < SIDELIMIT && frontDistancer < FRONTLIMIT) {
     turn_left(moveSpeed);
 
   }
+  ///////////////////////////////////////IF STATEMENT 9//////////////////////////////////////////////////
 
+  else if (MlDistance < CORNERLIMIT && leftDistance < SIDELIMIT && frontDistancel < FRONTLIMIT) {
+    turn_right(moveSpeed);
+  }
+  ///////////////////////////////////////IF STATEMENT 10 - if front two blocked, use random number generator to move a random direction//////////////////////////////////////////////////
   else if (frontDistancer < FRONTLIMIT && frontDistancel < FRONTLIMIT) {
-    Serial.println("front is bkocked");
     int randNumber = random(0, 10);
     bool inloop = 1;
     if (randNumber % 2 == 0) {
       while ((inloop == 1) && (frontDistancer < FRONTLIMIT) && (frontDistancel < FRONTLIMIT)) {
         frontDistancer = frontr.getDistance();
         frontDistancel = frontl.getDistance();
-        //        Serial.print(" front right: ");
-        //        Serial.print(frontDistancer);
-        //        Serial.print(" front left: ");
-        //        Serial.print(frontDistancel);
         turn_right(moveSpeed);
       }
       inloop = 0;
     }
     else if (randNumber % 2 == 1) {
       while ((inloop == 1) && (frontDistancer < FRONTLIMIT) && (frontDistancel < FRONTLIMIT)) {
-        //        Serial.print(" front right: ");
-        //        Serial.print(frontDistancer);
-        //        Serial.print(" front left: ");
-        //        Serial.print(frontDistancel);
         frontDistancer = frontr.getDistance();
         frontDistancel = frontl.getDistance();
         turn_left(moveSpeed);
@@ -268,165 +284,166 @@ void loop() {
     }
   }
 
-
-  else if (leftDistance < SIDELIMIT) {
-    turn_right(moveSpeed);
-  }
-
-  else if (MlDistance < CORNERLIMIT) {
-    turn_right(moveSpeed);
-
-  }
-  else if (MlDistance < CORNERLIMIT && leftDistance < SIDELIMIT && frontDistancel < FRONTLIMIT) {
-    turn_right(moveSpeed);
-  }
+  /********************************************************************************************************************/
 
   BNO::sEulAnalog_t   sEul;
   sEul = bno.getEul();
   newYaw = sEul.head;
+  roll = sEul.roll;
+  Serial.print("Roll");
+  Serial.print(roll);
 
-  //  Serial.print("Old Yaw = ");
-  //  Serial.print(oldYaw);
-  //  Serial.print("New Yaw = ");
-  //  Serial.println(newYaw);
-  //  Serial.println("Watchdogln = ");
-  //  Serial.println(watchdogtimer);
+  if (abs(sEul.roll) > 5) {
+    Serial.println("RAMP, GO DOWN");
+    go_back();
+    delay(600);
+    turn_left(moveSpeed);
+    delay (600);
+
+  }
 
   if ((newYaw - oldYaw) > 15) {
     oldYaw = newYaw;
     watchdogtimer = 0;
   }
-  if (watchdogtimer > 500) {
+  if (watchdogtimer > 350) {
     Serial.println("WATCHDOG is Activated");
     go_back();
     delay(400);
     turn_right(moveSpeed);
-    delay(400);
+    delay(800);
     watchdogtimer = 0;
 
   }
-
-
   watchdogtimer += 1;
-  //  if (watchdog > 100) {
-  //    Serial.println("STUCK"); // can swap code here to make not stuck
-  //  }
-  //
-  //  if (frontDistancel > 20 && frontDistancer > 20 && backflag == 0) {
-  //
-  //    // Check if left sensor detects something, turn right if so
-  //    if (leftDistance < 15 && rightDistance > 15)  {
-  //      turn_right(moveSpeed);
-  //      //      Serial.println(micros());
-  //      if (MlDistance < 15)  {
-  //        turn_right(moveSpeed);
-  //        //      Serial.println(micros());
-  //      }
-  //    }
-  //    else if (rightDistance < 15 && leftDistance > 15)  {
-  //      turn_left(moveSpeed);
-  //
-  //    }
-  //
-  //    else {
-  //      go_forward(moveSpeed);
-  //    }
-  //
-  //  }
-  //
-  //  // CASE If either front sensor detect somthing
-  //  else if (frontDistancer < 20 || frontDistancel < 20) {
-  //    //    rightDistance = right.getDistance();
-  //    //    leftDistance = left.getDistance();
-  //
-  //    // Check if left sensor detects something, turn right if so
-  //    if (leftDistance < 15 && rightDistance > 15 && backflag == 0)  {
-  //      turn_right(moveSpeed);
-  //
-  //    }
-  //
-  //    if (rightDistance < 15 && leftDistance > 15 && backflag == 0)  {
-  //      turn_left(moveSpeed);
-  //
-  //    }
-  //
-  //    // If left and right sensor detect nothing, turn left (CHANGE THIS LATER)
-  //    if (rightDistance > 15 && leftDistance > 15 && backflag == 0 )  {
-  //      turn_right(moveSpeed);
-  //    }
-  //
-  //    // If both left and right sensor detects something
-  //    if (rightDistance < 15 && leftDistance < 15 && backflag == 0)  {
-  //      backflag = 1;
-  //    }
-  //  }
-  //
-  //  else if (backflag == 1) {
-  //    while (rightDistance < 15 || leftDistance < 15) {
-  //      go_back();
-  //      rightDistance = right.getDistance();
-  //      leftDistance = left.getDistance();
-  //    }
-  //
-  //    backflag = 0;
-  //    turn_right(moveSpeed);
-  //    delay(3000);
-  //
-  //  }
 
+  if (timecounter % 80 == 0 && timeboi > 50000) {
 
+    //    tcs.getRawData(&r, &g, &b, &c);
+    if (r < redUp && g < greenUp && b < blueUp && r > redLow && g > greenLow && b > blueLow) {
+      onBase == true;
+    }
+  }
 
-  //    if (timecounter %15 == 0) {
-  //    tcs.getRawData(&r, &g, &b, &c);
-  //    colorTemp = tcs.calculateColorTemperature(r, g, b);
-  //      Serial.print("COLOUR IS READ ");
-  //
-  //    }
-  //    if (r > g && r > b && base == 1) {
-  //
-  //    }
-  //    if (g > r && g > b && base == 0) {
-  //
-  //    }
-
-  //    Serial.print("Color Temp: "); Serial.print(colorTemp, DEC); Serial.print(" K - ");
-  //    Serial.print("Lux: "); Serial.print(lux, DEC); Serial.print(" - ");
-  //    Serial.print("R: "); Serial.print(r, DEC); Serial.print(" ");
-  //    Serial.print("G: "); Serial.print(g, DEC); Serial.print(" ");
-  //    Serial.print("B: "); Serial.print(b, DEC); Serial.print(" ");
-  //    Serial.print("C: "); Serial.print(c, DEC); Serial.print(" ");
-  //  Serial.println(" ");
-  //  timecounter += 1;
-  //  //  Serial.print("timecounter ");
-  //  //  Serial.print(timecounter);
-  //  Serial.println(timecounter);
-
-
-  tcs.getRawData(&r, &g, &b, &c);
-
- if 
-
-
- int redUP = r + colourdiff;
-int greenUp = g + colourdiff;
-int blueUp = b + colourdiff;
-int redLow = r - colourdiff;
-int greenLow = g - colourdiff;
-int blueLow = b - colourdiff;
-
-
-
-  if (timeboi > 100000 && colourUpLim == true && colourLowLim==true) {
+  if (timeboi > 100000 && onBase == true) {
     while (1) {
       Serial.println("BASE");
       stationary();
     }
   }
-  if (timeboi > 130010) {
-    while (1) {
-      Serial.println("STOP");
-      go_forward(0);
-    }
-  }
-
+  
+Serial.println(timeboi);
 }
+
+///////////////////////////////////////////////////////////////////////////////END CODE////////////////////////////////////
+//    if (timecounter %15 == 0) {
+//    tcs.getRawData(&r, &g, &b, &c);
+//    colorTemp = tcs.calculateColorTemperature(r, g, b);
+//      Serial.print("COLOUR IS READ ");
+//
+//    }
+//    if (r > g && r > b && base == 1) {
+//
+//    }
+//    if (g > r && g > b && base == 0) {
+//
+//    }
+//
+//    Serial.print("Color Temp: "); Serial.print(colorTemp, DEC); Serial.print(" K - ");
+//    Serial.print("Lux: "); Serial.print(lux, DEC); Serial.print(" - ");
+//    Serial.print("R: "); Serial.print(r, DEC); Serial.print(" ");
+//    Serial.print("G: "); Serial.print(g, DEC); Serial.print(" ");
+//    Serial.print("B: "); Serial.print(b, DEC); Serial.print(" ");
+//    Serial.print("C: "); Serial.print(c, DEC); Serial.print(" ");
+//  Serial.println(" ");
+//  timecounter += 1;
+//  //  Serial.print("timecounter ");
+//  //  Serial.print(timecounter);
+//  Serial.println(timecounter);
+//
+//  Serial.print("Old Yaw = ");
+//  Serial.print(oldYaw);
+//  Serial.print("New Yaw = ");
+//  Serial.println(newYaw);
+//  Serial.println("Watchdogln = ");
+//  Serial.println(watchdogtimer);
+//
+//  if (watchdog > 100) {
+//    Serial.println("STUCK"); // can swap code here to make not stuck
+//  }
+//
+//  if (frontDistancel > 20 && frontDistancer > 20 && backflag == 0) {
+//
+//    // Check if left sensor detects something, turn right if so
+//    if (leftDistance < 15 && rightDistance > 15)  {
+//      turn_right(moveSpeed);
+//      //      Serial.println(micros());
+//      if (MlDistance < 15)  {
+//        turn_right(moveSpeed);
+//        //      Serial.println(micros());
+//      }
+//    }
+//    else if (rightDistance < 15 && leftDistance > 15)  {
+//      turn_left(moveSpeed);
+//
+//    }
+//
+//    else {
+//      go_forward(moveSpeed);
+//    }
+//
+//  }
+//
+//  // CASE If either front sensor detect somthing
+//  else if (frontDistancer < 20 || frontDistancel < 20) {
+//    //    rightDistance = right.getDistance();
+//    //    leftDistance = left.getDistance();
+//
+//    // Check if left sensor detects something, turn right if so
+//    if (leftDistance < 15 && rightDistance > 15 && backflag == 0)  {
+//      turn_right(moveSpeed);
+//
+//    }
+//
+//    if (rightDistance < 15 && leftDistance > 15 && backflag == 0)  {
+//      turn_left(moveSpeed);
+//
+//    }
+//
+//    // If left and right sensor detect nothing, turn left (CHANGE THIS LATER)
+//    if (rightDistance > 15 && leftDistance > 15 && backflag == 0 )  {
+//      turn_right(moveSpeed);
+//    }
+//
+//    // If both left and right sensor detects something
+//    if (rightDistance < 15 && leftDistance < 15 && backflag == 0)  {
+//      backflag = 1;
+//    }
+//  }
+//
+//  else if (backflag == 1) {
+//    while (rightDistance < 15 || leftDistance < 15) {
+//      go_back();
+//      rightDistance = right.getDistance();
+//      leftDistance = left.getDistance();
+//    }
+//
+//    backflag = 0;
+//    turn_right(moveSpeed);
+//    delay(3000);
+//
+//  }
+
+//  Serial.print("right: ");
+//  Serial.print(rightDistance);
+//  Serial.print(" left: ");
+//  Serial.print(leftDistance);
+//  Serial.print(" front right: ");
+//  Serial.print(frontDistancer);
+//  Serial.print(" front left: ");
+//  Serial.print(frontDistancel);
+//  Serial.print("Mid Left: ");
+//  Serial.print(MlDistance);
+//  Serial.print(", ");
+//  Serial.print("Mid right: ");
+//  Serial.println(MrDistance);
